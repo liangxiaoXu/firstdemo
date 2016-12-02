6567db98688e936316702125c84714c6bff07cd3
xuliangxiao <xuliangxiao@jd.com>
2016-12-02 18:19:45 +0800

    调用内容提供者，选择联系人，主activity回调获取选择的信息

--- src/com/xlx/firstdemo/Activity/FirstActivity.java
+++ src/com/xlx/firstdemo/Activity/FirstActivity.java
@@ -2,7 +2,11 @@ package com.xlx.firstdemo.Activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract.Contacts;
+import android.provider.SyncStateContract.Constants;
+import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -32,9 +36,38 @@ public class FirstActivity extends Activity {
 			}
 		});
         
+        //选择一个联系人
+        final Button selectContact = (Button) findViewById(R.id.selectContactId);
+        selectContact.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View paramView) {
+			    Intent intent = new Intent(Intent.ACTION_PICK, Contacts.CONTENT_URI);
+			    startActivityForResult(intent, 1);
+			}
+		});
+        
     }
     
-    //显示打开activity
+    /**
+     * 回调，取数据
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//    	super.onActivityResult(requestCode, resultCode, data);
+    	if(resultCode == Activity.RESULT_OK &&
+    			requestCode == 1 ){
+    		Cursor cursor = getContentResolver().query(data.getData(), new String[]{Contacts.DISPLAY_NAME}, 
+    				null, null, null);
+    		if(cursor.moveToFirst()){
+    			int columnIndex = cursor.getColumnIndex(Contacts.DISPLAY_NAME);
+    			String name = cursor.getString(columnIndex);
+    			Toast t = Toast.makeText(getApplicationContext(), "你选择的联系人名字是:" + name, Toast.LENGTH_LONG);
+    			t.show();
+    		}
+    	}
+    	
+    }
+    
+    //显式打开activity
 //    public void openSecondActivity(View view){
 //      Intent intent = new Intent(this, SecondActivity.class);
 //      startActivity(intent);
